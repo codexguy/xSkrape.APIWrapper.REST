9d8b296dd7200aa13677d6087fcd5e6e33be08e4
joel <admin@codexframework.com>
2017-10-30 19:18:21 -0700

    minor updates

--- xSkrape.APIWrapper.REST.Sample/Program.cs
+++ xSkrape.APIWrapper.REST.Sample/Program.cs
@@ -12,7 +12,7 @@ namespace APIWrapperDemo
         static void Main(string[] args)
         {
             // Substitute your own client key, obtained by visiting https://www.xskrape.com, registering and adding a client query end-point
-            // Credit usage is explained more here: https://www.xskrape.com/home/addcredits - usage is generally FREE up until a certain level of usage, or in the case of the in-process libraries (fixed charge per machine per month), your first month per machine can often be covered by free monthly credits as a "trial" period
+            // Credit usage is explained more here: https://www.xskrape.com/home/products - usage is generally FREE up until a certain level of usage, or in the case of the in-process libraries (fixed charge per machine per month), your first month per machine can often be covered by free monthly credits as a "trial" period
             // Some examples below require your own client keys, etc. and may not return successful results without these being substituted in the code below (we have tested all examples with our own keys and got passes)
             // Something on your wish list? Have your own unique use cases? Contact me, community@codexframework.com
             // Other useful links:
@@ -26,7 +26,7 @@ namespace APIWrapperDemo
             RESTExamples.Example4_MergeRequestFromWebAPIReturningShapedJSON().Wait();
             RESTExamples.Example5_SingleValueFromRSSFeed().Wait();
             RESTExamples.Example6_MergeMultipleRSSRequestsExtractingByLineNumber().Wait();
-            RESTExamples.Example7_ExtractMultipleElementsFromSinglePage().Wait();           // todo - good one to test date parsing further
+            RESTExamples.Example7_ExtractMultipleElementsFromSinglePage().Wait();
             RESTExamples.Example8_IsolateTabularDataOfInterestUsingTableIdentificationExpression().Wait();
             RESTExamples.Example9_ExtractAndFlattenJSONOnHtmlPage().Wait();
             RESTExamples.Example10_ExtractDataFromAnExcelFileLocatedInAZipFileStoredOnDropBox().Wait();
